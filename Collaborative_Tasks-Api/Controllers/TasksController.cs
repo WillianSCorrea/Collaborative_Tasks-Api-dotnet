2a78297471f6f43d5f1b2e42707f3c6fc6032cad
Willian da Silva Correa <163740121+WillianSCorrea@users.noreply.github.com>
2025-12-18 14:49:40 +0000

    Adding the solution file

--- Collaborative_Tasks-Api/Controllers/TasksController.cs
+++ Collaborative_Tasks-Api/Controllers/TasksController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Collaborative_Tasks.Services;
 using Collaborative_Tasks.DTOs;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using System.Threading.Tasks;
 namespace Collaborative_Tasks.Controllers
